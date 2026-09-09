df7e66b302fb9be268696bf43728f7ad65c5799b
luisangelbermudez <luisangelbermudez@gmail.com>
2026-09-08 20:47:14 -0500

    Update app.js

--- portal_autogestion/Backend/src/app.js
+++ portal_autogestion/Backend/src/app.js
@@ -37,8 +37,7 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor Express corriendo en http://localhost:${PORT}`);
-  verificarConexion();
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
 
 module.exports = app;
